9190f272a5472ba27cfcd8ce033f3ddc5408fae4
Corey Farwell <coreyf@rwell.org>
2011-11-26 12:35:36 -0800

    added licensing and navigation apps

--- openbrain/settings.py
+++ openbrain/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/coreyf/dev/openbrain/db.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': 'db.sqlite',                    # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -117,6 +117,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'core',
+    'navigation',
+    'licensing',
     # Uncomment the next line to enable the admin:
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
